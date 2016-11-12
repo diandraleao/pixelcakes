612a770e153e11a4b6d95542a927f780890d1fad
fabianamoraes <fabianacorream@hotmail.com>
2016-11-12 22:19:15 +0000

    nav w right routes

--- Handler/Usuario.hs
+++ Handler/Usuario.hs
@@ -21,7 +21,7 @@ getLoginR :: Handler Html
 getLoginR = do
     (widget,enctype)<- generateFormPost formUser
     defaultLayout $ do
-        setTitle "Adiministrator | Pixel Cakes"
+        setTitle "Administrator | Pixel Cakes"
         addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
         addStylesheetRemote "https://fonts.googleapis.com/css?family=Raleway"
         addStylesheet $ StaticR css_main_css
@@ -36,13 +36,84 @@ getLoginR = do
 
 getUsuarioR :: Handler Html
 getUsuarioR = do
-    (widget,enctype)<- generateFormPost formUser
-    defaultLayout $ do
-        [whamlet|
-            <form action=@{UsuarioR} method=post enctype=#{enctype}>
-                ^{widget}
-                <input type="submit" value="Cadastrar">
-        |]
+        (widget,enctype)<- generateFormPost formUser
+        defaultLayout $ do
+                setTitle "Administrator | Pixel Cakes"
+                addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+                addStylesheetRemote "https://fonts.googleapis.com/css?family=Raleway"
+                addStylesheet $ StaticR css_main_css
+                addScriptRemote "https://code.jquery.com/jquery-3.1.1.min.js"
+                addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                addScript $ StaticR js_main_js
+                [whamlet|
+                    <div class="container">
+                        <img src=@{StaticR img_logo_png} alt="Logo Pixel Cakes" class="center-block" id="imglogin">
+                        <nav class="navbar navbar-default" id="navbar-admin">
+                            <div class="container-fluid">
+                                <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                        <span class="sr-only">Navegação alternativa
+                                        <span class="icon-bar">
+                                        <span class="icon-bar">
+                                        <span class="icon-bar">
+        
+                                <div id="navbar" class="navbar-collapse collapse">
+                                    <ul class="nav navbar-nav">
+                                        <li class="active">
+                                            <a href=@{PerfilR}>home
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                serviços <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{ServicoR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href=@{ListServR}>
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                produtos <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{ProdutoR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href=@{ListProdR}>
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
+                                        <li>
+                                            <a href=@{ListContR}>contatos
+                                        <li class="dropdown active">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                usuários <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{UsuarioR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href="#">
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
+                                    <ul class="nav navbar-nav navbar-right">
+                                        <li>
+                                            <form action=@{LogoutR} method=post>
+                                                <input type="submit" value="sair" class="btn-sair">
+                                            
+                        <form action=@{UsuarioR} method=post enctype=#{enctype}>
+                            ^{widget}
+                            <input type="submit" value="Cadastrar" class="btn btn-default">
+            |]
 
 postUsuarioR :: Handler Html
 postUsuarioR = do
@@ -50,8 +121,81 @@ postUsuarioR = do
     case resultado of
         FormSuccess user -> do
             uid <- runDB $ insert user
-            defaultLayout [whamlet|
-                Usuárix cadastrado com e-mail #{usuarioEmail user}
+            defaultLayout $ do
+                setTitle "Administrator | Pixel Cakes"
+                addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+                addStylesheetRemote "https://fonts.googleapis.com/css?family=Raleway"
+                addStylesheet $ StaticR css_main_css
+                addScriptRemote "https://code.jquery.com/jquery-3.1.1.min.js"
+                addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                addScript $ StaticR js_main_js
+                [whamlet|
+                    <div class="container">
+                        <img src=@{StaticR img_logo_png} alt="Logo Pixel Cakes" class="center-block" id="imglogin">
+                        <nav class="navbar navbar-default" id="navbar-admin">
+                            <div class="container-fluid">
+                                <div class="navbar-header">
+                                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                                        <span class="sr-only">Navegação alternativa
+                                        <span class="icon-bar">
+                                        <span class="icon-bar">
+                                        <span class="icon-bar">
+        
+                                <div id="navbar" class="navbar-collapse collapse">
+                                    <ul class="nav navbar-nav">
+                                        <li>
+                                            <a href=@{PerfilR}>home
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                serviços <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{ServicoR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href=@{ListServR}>
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                produtos <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{ProdutoR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href=@{ListProdR}>
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
+                                        <li>
+                                            <a href=@{ListContR}>contatos
+                                        <li class="dropdown active">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                usuários <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{UsuarioR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href="#">
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
+                                    <ul class="nav navbar-nav navbar-right">
+                                        <li>
+                                            <form action=@{LogoutR} method=post>
+                                                <input type="submit" value="sair" class="btn-sair">
+                                            
+                        <h2 class="alert alert-success text-center">Usuário cadastrado com sucesso!
+                    <p class="text-uppercase text-center">E-mail utilizado: #{usuarioEmail user}
             |]
         _ -> redirect HomeR
 
@@ -77,10 +221,13 @@ getPerfilR = do
         Just str -> do
             usuario <- runDB $ get404 (read (unpack str))
             defaultLayout $ do
-                setTitle "Adiministrator | Pixel Cakes"
+                setTitle "Administrator | Pixel Cakes"
                 addStylesheetRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
                 addStylesheetRemote "https://fonts.googleapis.com/css?family=Raleway"
                 addStylesheet $ StaticR css_main_css
+                addScriptRemote "https://code.jquery.com/jquery-3.1.1.min.js"
+                addScriptRemote "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
+                addScript $ StaticR js_main_js
                 [whamlet|
                     <div class="container">
                         <img src=@{StaticR img_logo_png} alt="Logo Pixel Cakes" class="center-block" id="imglogin">
@@ -95,16 +242,56 @@ getPerfilR = do
         
                                 <div id="navbar" class="navbar-collapse collapse">
                                     <ul class="nav navbar-nav">
+                                        <li class="active">
+                                            <a href=@{PerfilR}>home
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                serviços <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{ServicoR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href=@{ListServR}>
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                produtos <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{ProdutoR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href=@{ListProdR}>
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
                                         <li>
-                                            <a href="#">home
+                                            <a href=@{ListContR}>contatos
+                                        <li class="dropdown">
+                                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                                usuários <span class="caret">
+                                            <ul class="dropdown-menu">
+                                                <li>
+                                                    <a href=@{UsuarioR}>
+                                                        <span class="glyphicon glyphicon-plus"></span>
+                                                        adicionar
+                                                
+                                                <li role="separator" class="divider"></li>
+                                                <li>
+                                                    <a href="#">
+                                                        <span class="glyphicon glyphicon-th-list"></span>
+                                                        ver todos
+                                    <ul class="nav navbar-nav navbar-right">
                                         <li>
-                                            <a href="#">serviços
-                                        <li>
-                                            <a href="#">produtos
-                                        <li>
-                                            <a href="#">contatos
-                                        <li>
-                                            <a href="#">usuários
+                                            <form action=@{LogoutR} method=post>
+                                                <input type="submit" value="sair" class="btn-sair">
 
                         <article id="logado" class="">
                             <h1> Bem-vindo!
